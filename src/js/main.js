63c4038600c9537b264cd0e3949e18a83ef7d9d4
ssainty <aauzinger@yandex.ru>
2023-01-19 12:36:58 +0300

    added function for task creation, adding of controls button replaced to function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,44 @@
+function createCheckButton(button) {
+  button.addEventListener("click", () => {
+    button.parentNode.classList.toggle("task_complete");
+  });
+}
+
+function createDeleteButton(button) {
+  button.addEventListener("click", () => {
+    button.parentNode.remove();
+  });
+}
+
+function createTask(taskText) {
+  const task = document.createElement("li");
+  task.classList.add("todo__item", "task");
+
+  const checkButton = document.createElement("button");
+  checkButton.classList.add("task__status");
+  createCheckButton(checkButton);
+
+  const text = document.createElement("p");
+  text.classList.add("task__message");
+  text.innerText = taskText;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("task__delete-button");
+  createDeleteButton(deleteButton);
+
+  const deleteButtonImg = document.createElement("img");
+  deleteButtonImg.classList.add("task__delete-img");
+  deleteButtonImg.setAttribute("src", "images/delete.svg");
+  deleteButtonImg.setAttribute("alt", "Крест кнопки удаления");
+  deleteButton.appendChild(deleteButtonImg);
+
+  task.appendChild(checkButton);
+  task.appendChild(text);
+  task.appendChild(deleteButton);
+
+  return task;
+}
+
 function init() {
   const form = document.getElementById("form");
   const input = document.getElementById("input");
@@ -6,24 +47,8 @@ function init() {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const taskText = input.value;
-    const taskHTML = `<li class="todo__item task">
-    <button class="task__status"></button>
-    <p class="task__message">${taskText}</p>
-    <button class="task__delete-button">
-    <img class="task__delete-img" src="images/delete.svg" alt="Крест кнопки удаления">
-    </button>
-    </li>`;
-
-    tasksList.insertAdjacentHTML("beforeend", taskHTML);
+    tasksList.appendChild(createTask(input.value));
     input.value = "";
-  });
-
-  const checkButtons = document.getElementsByClassName("task__status");
-  [...checkButtons].forEach((button) => {
-    button.addEventListener("click", () => {
-      button.parentNode.classList.toggle("task_complete");
-    });
   });
 }
 
